3a5b63564b07290c556b68bcec6ba1bfb1a31573
adrihui <huideadriana@gmail.com>
2021-05-13 00:29:00 -0300

    agrega resta

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const {sumar,restar,multiplicar,dividir} = require('./data/calculadora')
 
 switch (process.arg[2]) {
-    case sumar:
+    case "sumar":
         sumar(process.argv[3],process.argv[4])
+    break;
+    case "restar":
+        restar(process.argv[3],process.argv[4])
         break;
-
     default:
         break;
 }
